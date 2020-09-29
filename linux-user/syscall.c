2469c4f952a43f7793c02d7d713aff2e08f7795e
Alessandro Di Federico <ale@rev.ng>
2020-09-29 12:13:09 +0200

    tmp

--- linux-user/syscall.c
+++ linux-user/syscall.c
@@ -5587,7 +5587,7 @@ void on_exit_syscall(void);
 /* do_syscall() should always have a single exit point at the end so
    that actions, such as logging of syscall results, can be performed.
    All errnos that do_syscall() returns must be -TARGET_<errcode>. */
-abi_long do_syscall(void *cpu_env, int num, abi_long arg1,
+abi_long do_syscall(CPUArchState *cpu_env, int num, abi_long arg1,
                     abi_long arg2, abi_long arg3, abi_long arg4,
                     abi_long arg5, abi_long arg6, abi_long arg7,
                     abi_long arg8)
