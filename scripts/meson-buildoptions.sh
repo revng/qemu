fe6fc7811951bfb1dbfc7f69251ccb6bbca10f52
Alessandro Di Federico <ale@rev.ng>
2025-10-09 18:10:01 +0200

    Introduce libtcg targets
    These are copied as is from `*-linux-user` targets.

--- scripts/meson-buildoptions.sh
+++ scripts/meson-buildoptions.sh
@@ -36,6 +36,8 @@ meson_options_help() {
   printf "%s\n" '                           (choices: auto/disabled/enabled/internal/system)'
   printf "%s\n" '  --enable-fuzzing         build fuzzing targets'
   printf "%s\n" '  --enable-gcov            Enable coverage tracking.'
+  printf "%s\n" '  --enable-libtcg          Build *-user target as a shared library, exposing'
+  printf "%s\n" '                           tcg frontend'
   printf "%s\n" '  --enable-lto             Use link time optimization'
   printf "%s\n" '  --enable-malloc=CHOICE   choose memory allocator to use [system] (choices:'
   printf "%s\n" '                           jemalloc/system/tcmalloc)'
